54c9603290f632c4e7b5caf4269123b615c1cbfe
Rajasegar Chandran <rajasegar.c@gmail.com>
2025-05-11 12:04:37 +0530

    chore: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hangaroundtheweb.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://hangaroundtheweb.com',
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
